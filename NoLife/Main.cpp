6f445d348f552e9a8497cb7778bf460c56a5a4c2
Karol Scipniak <karol.scipniak@gmail.com>
2014-05-01 18:20:16 +0200

    fully working simple blender object loader

--- NoLife/Main.cpp
+++ NoLife/Main.cpp
@@ -8,9 +8,16 @@
 #include "shaderprogram.h"
 #include "cube.h"
 #include "teapot.h"
+#include "vector"
+#include <ios>
+#include <sstream>
 
 
+using namespace std;
 
+vector<float> suzanne_vertices;
+vector<float> suzanne_normals;
+vector<GLushort> suzanne_elements;
 
 //Macierze
 glm::mat4  matP;//rzutowania
@@ -37,21 +44,171 @@ GLuint vao;
 GLuint bufVertices; //Uchwyt na bufor VBO przechowuj¹cy tablicê wspó³rzêdnych wierzcho³ków
 GLuint bufColors;  //Uchwyt na bufor VBO przechowuj¹cy tablicê kolorów
 GLuint bufNormals; //Uchwyt na bufor VBO przechowuj¹cy tablickê wektorów normalnych
+GLuint bufIndex;
 
 //"Model" który rysujemy. Dane wskazywane przez poni¿sze wskaŸniki i o zadanej liczbie wierzcho³ków s¹ póŸniej wysowane przez program.
 //W programie s¹ dwa modele, z których jeden mo¿na wybraæ komentuj¹c/odkomentowuj¹c jeden z poni¿szych fragmentów.
 
 //Kostka
-float *vertices = cubeVertices;
+/*float *vertices = cubeVertices;
 float *colors = cubeColors;
 float *normals = cubeNormals;
-int vertexCount = cubeVertexCount;
+int vertexCount = cubeVertexCount;*/
 
+
+float *vertices;
+float *colors;
+float *normals;
+int vertexCount;
+GLushort *elements;
+
+/*
 //Czajnik
-/*float *vertices=teapotVertices;
+float *vertices=teapotVertices;
 float *colors=teapotColors;
 float *normals=teapotNormals;
 int vertexCount=teapotVertexCount;*/
+
+//³adowanie modelu z Blendera
+void load_obj(const char* filename, vector<float> &vertices, vector<float> &normals) {
+	vector<int> elements;
+	vector<glm::vec4> verts;
+	ifstream in(filename, ios::in);
+	if (!in) { cerr << "Cannot open " << filename << endl; exit(1); }
+
+	string line;
+	while (getline(in, line)) {
+		if (line.substr(0, 2) == "v ") {
+			
+			istringstream s(line.substr(2));
+			glm::vec4 v; s >> v.x; s >> v.y; s >> v.z; v.w = 1.0f;
+			verts.push_back(v);
+		}
+		else if (line.substr(0, 2) == "f ") {
+			istringstream s(line.substr(2));
+			GLushort a, b, c;
+			s >> a; s >> b; s >> c;
+			a--; b--; c--;
+			elements.push_back(a); elements.push_back(b); elements.push_back(c);
+		}
+		else if (line[0] == '#') { /* ignoring this line */ }
+		else { /* ignoring this line */ }
+
+		
+
+	
+	}
+
+	for (int i = 0; i < elements.size(); i++)
+	{
+		vertices.push_back((float) verts[elements[i]].x);
+		vertices.push_back((float) verts[elements[i]].y);
+		vertices.push_back((float) verts[elements[i]].z);
+		vertices.push_back((float) verts[elements[i]].w);
+
+
+		if (i % 3 == 0)
+		{
+			glm::vec4 a = verts[elements[i+1]] - verts[elements[i]];
+			glm::vec4 b = verts[elements[i + 2]] - verts[elements[i]];
+			
+			glm::vec4 n = glm::vec4(glm::normalize(glm::cross(glm::vec3(a.x,a.y,a.z), glm::vec3(b.x,b.y,b.z))), 0.0f);
+
+			for (int j = 0; j < 3; j++)
+			{
+				normals.push_back(n.x);
+				normals.push_back(n.y);
+				normals.push_back(n.z);
+				normals.push_back(n.w);
+			}
+
+		}
+
+		
+
+	}
+
+	cout << elements.size() << endl << endl;
+}
+
+
+
+void alternativeSetup()
+{
+	glGenBuffers(1, &bufVertices);
+	GLuint location = shaderProgram->getAttribLocation("vertex");
+	glEnableVertexAttribArray(location);
+	glEnableClientState(GL_VERTEX_ARRAY);
+	// Describe our vertices array to OpenGL (it can't guess its format automatically)
+	glBindBuffer(GL_ARRAY_BUFFER, bufVertices);
+	glVertexAttribPointer(
+		location,  // attribute
+		4,                  // number of elements per vertex, here (x,y,z,w)
+		GL_FLOAT,           // the type of each element
+		GL_FALSE,           // take our values as-is
+		0,                  // no extra data between each position
+		0                   // offset of first element
+		);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(*vertices), vertices, GL_STREAM_DRAW);
+
+
+	glGenBuffers(1, &bufNormals);
+	GLuint location2 = shaderProgram->getAttribLocation("normal");
+	glEnableClientState(GL_NORMAL_ARRAY);
+	//glEnableNormalsAttribArray(location2);
+	glBindBuffer(GL_ARRAY_BUFFER, bufNormals);
+	glVertexAttribPointer(
+		location2, // attribute
+		3,                  // number of elements per vertex, here (x,y,z)
+		GL_FLOAT,           // the type of each element
+		GL_FALSE,           // take our values as-is
+		0,                  // no extra data between each position
+		0                   // offset of first element
+		);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(*normals), normals, GL_STREAM_DRAW);
+	
+
+	glGenBuffers(1, &bufIndex);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, bufIndex);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(elements), elements, GL_STREAM_DRAW);
+
+
+}
+
+void alternativeSetup2()
+{
+	glGenBuffers(1, &bufVertices);
+	glBindBuffer(GL_ARRAY_BUFFER, bufVertices);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STREAM_DRAW);
+
+	glGenBuffers(1, &bufIndex);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, bufIndex);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(elements), elements, GL_STREAM_DRAW);
+}
+
+void alternativeDraw()
+{
+	glBindBuffer(GL_ARRAY_BUFFER, bufVertices);
+
+	glVertexPointer(3, GL_FLOAT, sizeof(glm::vec3), 0);
+	glEnableClientState(GL_VERTEX_ARRAY);
+
+	glNormalPointer(GL_FLOAT, sizeof(glm::vec3), (void*)(sizeof(glm::vec3)));
+	glEnableClientState(GL_NORMAL_ARRAY);
+
+	glClientActiveTexture(GL_TEXTURE0);
+	glTexCoordPointer(2, GL_FLOAT, sizeof(glm::vec2), (void*)(sizeof(glm::vec3) + sizeof(glm::vec3)));
+	glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, bufIndex);
+
+
+		glDrawElements(GL_TRIANGLES, sizeof(elements)/sizeof(GLushort), GL_UNSIGNED_INT, 0);
+
+		glDisableClientState(GL_VERTEX_ARRAY);
+		glDisableClientState(GL_NORMAL_ARRAY);
+		glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+}
 
 
 //Procedura rysuj¹ca jakiœ obiekt. Ustawia odpowiednie parametry dla vertex shadera i rysuje.
@@ -87,7 +244,7 @@ void drawObject() {
 //Procedura rysuj¹ca
 void displayFrame() {
 	//Wyczyœæ bufor kolorów i bufor g³êbokoœci
-	glClearColor(0, 0, 0, 1);
+	glClearColor(1, 0, 0, 1);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	//Wylicz macierz rzutowania
@@ -101,10 +258,13 @@ void displayFrame() {
 
 	//Narysuj obiekt
 	drawObject();
+	//alternativeDraw();
 
 	//Tylny bufor na przedni
 	glutSwapBuffers();
 }
+
+
 
 GLuint makeBuffer(void *data, int vertexCount, int vertexSize) {
 	GLuint handle;
@@ -112,15 +272,22 @@ GLuint makeBuffer(void *data, int vertexCount, int vertexSize) {
 	glGenBuffers(1, &handle);//Wygeneruj uchwyt na Vertex Buffer Object (VBO), który bêdzie zawiera³ tablicê danych
 	glBindBuffer(GL_ARRAY_BUFFER, handle);  //Uaktywnij wygenerowany uchwyt VBO 
 	glBufferData(GL_ARRAY_BUFFER, vertexCount*vertexSize, data, GL_STATIC_DRAW);//Wgraj tablicê do VBO
+	
+
 
 	return handle;
 }
 
+
+
 //Procedura tworz¹ca bufory VBO zawieraj¹ce dane z tablic opisuj¹cych rysowany obiekt.
 void setupVBO() {
 	bufVertices = makeBuffer(vertices, vertexCount, sizeof(float)* 4); //Wspó³rzêdne wierzcho³ków
-	bufColors = makeBuffer(colors, vertexCount, sizeof(float)* 4);//Kolory wierzcho³ków
+	//bufColors = makeBuffer(colors, vertexCount, sizeof(float)* 4);//Kolory wierzcho³ków
 	bufNormals = makeBuffer(normals, vertexCount, sizeof(float)* 4);//Wektory normalne wierzcho³ków
+
+	
+
 }
 
 void assignVBOtoAttribute(char* attributeName, GLuint bufVBO, int variableSize) {
@@ -139,7 +306,7 @@ void setupVAO() {
 	glBindVertexArray(vao);
 
 	assignVBOtoAttribute("vertex", bufVertices, 4); //"vertex" odnosi siê do deklaracji "in vec4 vertex;" w vertex shaderze
-	assignVBOtoAttribute("color", bufColors, 4); //"color" odnosi siê do deklaracji "in vec4 color;" w vertex shaderze
+	//assignVBOtoAttribute("color", bufColors, 4); //"color" odnosi siê do deklaracji "in vec4 color;" w vertex shaderze
 	assignVBOtoAttribute("normal", bufNormals, 4); //"normal" odnosi siê do deklaracji "in vec4 normal;" w vertex shaderze
 
 	glBindVertexArray(0);
@@ -201,6 +368,9 @@ void initOpenGL() {
 	setupShaders();
 	setupVBO();
 	setupVAO();
+	//alternativeSetup();
+	//alternativeSetup2();
+
 	glEnable(GL_DEPTH_TEST);
 }
 
@@ -221,6 +391,31 @@ void freeVAO() {
 
 
 int main(int argc, char** argv) {
+	load_obj("cube1.obj", suzanne_vertices, suzanne_normals);
+	
+	/*suzanne_normals.push_back(-1.0f);
+	suzanne_normals.push_back(0.0f);
+	suzanne_normals.push_back(0.0f);
+	suzanne_normals.push_back(0.0f);
+	suzanne_normals.push_back(-1.0f);
+	suzanne_normals.push_back(0.0f);
+	suzanne_normals.push_back(0.0f);
+	suzanne_normals.push_back(0.0f);
+	suzanne_normals.push_back(-1.0f);
+	suzanne_normals.push_back(0.0f);
+	suzanne_normals.push_back(0.0f);
+	suzanne_normals.push_back(0.0f);*/
+
+	vertices = &suzanne_vertices[0];
+	normals = &suzanne_normals[0];
+
+	/*for (int i = 0; i < 144; i+= 4)
+	{
+		cout << i << ": " << normals[i] << " " << normals[i + 1] << " " << normals[i + 2] << " " << normals[i + 3] << endl;
+	}*/
+
+	vertexCount = suzanne_vertices.size() / 4;
+
 	initGLUT(&argc, argv);
 	initGLEW();
 	initOpenGL();
